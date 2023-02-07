365b1345e4f99a111d81f79504da22a20453320c
HEYAHONG <2229388563@qq.com>
2023-02-07 10:25:16 +0800

    完善qscintillaedit头文件

--- qscintillaedit.h
+++ qscintillaedit.h
@@ -2,6 +2,9 @@
 #define QSCINTILLAEDIT_H
 
 #include "ScintillaEdit.h"
+#include "Scintilla.h"
+#include "ILexer.h"
+#include "Lexilla.h"
 
 //对ScintillaEdit宏定义,使其符合Qt的类规则（以Q开头）
 #ifndef QScintillaEdit
